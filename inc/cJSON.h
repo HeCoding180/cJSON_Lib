455a32eb6b6189609e307043636fb267de280040
HeCoding180 <HeCoding180@outlook.com>
2024-08-11 00:17:37 +0200

    Added implementation for generic object memory allocation. Changed cJSON_String_t type

--- inc/cJSON.h
+++ inc/cJSON.h
@@ -8,6 +8,8 @@
 
 #include <inttypes.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 //   ---   Defines   ---
 
@@ -17,25 +19,31 @@
 // Basic Typedefs
 
 /**
- * @brief Type defining the data type of the "size" value contained in cJSON data container structs.
+ * @brief   Type defining the data type of the "size" value contained in cJSON data container structs.
  * 
  */
-typedef uint32_t cJSON_object_size_t;
+typedef uint32_t cJSON_object_size_size_t;
 
 /**
- * @brief Type defining the data type for integer values saved in any cJSON data container structs.
+ * @brief   Type defining the data type for string values saved in any cJSON data container structs as well as the array of keys used in dictionaries.
+ * 
+ */
+typedef char* cJSON_String_t;
+
+/**
+ * @brief   Type defining the data type for integer values saved in any cJSON data container structs.
  * 
  */
 typedef int32_t cJSON_Int_t;
 
 /**
- * @brief Type defining the data type for floating point values saved in any cJSON data container structs.
+ * @brief   Type defining the data type for floating point values saved in any cJSON data container structs.
  * 
  */
 typedef float cJSON_Float_t;
 
 /**
- * @brief Type defining the data type for floating point values saved in any cJSON data container structs.
+ * @brief   Type defining the data type for floating point values saved in any cJSON data container structs.
  * 
  */
 typedef bool cJSON_Bool_t;
@@ -43,16 +51,18 @@ typedef bool cJSON_Bool_t;
 // Enum Typedefs
 
 /**
- * @brief cJSON data container structure type.
+ * @brief   cJSON data container structure type.
  * 
  */
 typedef enum cJSON_DataType
 {
+    Null = 0,
     Dictionary,
     List,
     String,
     Integer,
-    Float
+    Float,
+    Boolean
 } cJSON_DataType_t;
 
 typedef enum cJSON_Result
@@ -60,13 +70,14 @@ typedef enum cJSON_Result
     cJSON_Ok,
     cJSON_Datatype_Error,
     cJSON_Structure_Error,
+    cJSON_Not_Allocated_Error,
     cJSON_Unknown_Error
 } cJSON_Result_t;
 
 // Struct Typedefs
 
 /**
- * @brief cJSON generic data structure. Used to link values or list items to other data containers or values.
+ * @brief   Generic JSON object. Used to link data containers or values to lists or dictionaries they are contained in.
  * 
  */
 typedef struct cJSON_Generic
@@ -76,40 +87,39 @@ typedef struct cJSON_Generic
 } cJSON_Generic_t;
 
 /**
- * @brief cJSON data structure for a string object
- * 
- */
-typedef struct cJSON_String
-{
-    uint32_t length;
-    char *data;
-} cJSON_String_t;
-
-/**
- * @brief cJSON data structure for a list object
+ * @brief   cJSON data structure for a list object
  * 
  */
 typedef struct cJSON_List
 {
-    uint32_t length;
+    cJSON_object_size_size_t length;
     cJSON_Generic_t *data;
 } cJSON_List_t;
 
 /**
- * @brief cJSON dictionary key type.
+ * @brief   cJSON dictionary key type.
  * 
  */
 typedef cJSON_String_t cJSON_Key;
 
 /**
- * @brief cJSON data container for a dictionary object
+ * @brief   cJSON data container for a dictionary object
  * 
  */
 typedef struct cJSON_Dict
 {
-    uint32_t length;
+    cJSON_object_size_size_t length;
     cJSON_Key *keyData;
     cJSON_Generic_t *valueData;
 } cJSON_Dict_t;
 
 //   ---   Function Prototypes   ---
+
+/**
+ * @brief   Function that allocates the required memory for a cJSON_Generic_t object together with its specified data container specified in the containerType parameter.
+ * @param   containerType Specifies the type of the object stored in the generic object.
+ * @return  Returns the memory address of a newly allocated cJSON_Generic_t already containing the type and pointer to the object of the specified type.
+ */
+cJSON_Generic_t *cJSON_malloc_generic_object(cJSON_DataType_t containerType);
+
+
--- src/cJSON.c
+++ src/cJSON.c
@@ -8,3 +8,60 @@
  */
 
 #include "../inc/cJSON.h"
+
+//   ---   Private Function Prototypes   ---
+
+/**
+ * @brief   Function that allocates the required memory for a cJSON_Generic_t object together with its specified data container specified in the containerType parameter.
+ * @param   containerType Specifies the type of the object stored in the generic object.
+ * @return  Returns the memory address of a newly allocated cJSON_Generic_t already containing the type and pointer to the object of the specified type.
+ */
+cJSON_Generic_t *cJSON_malloc_generic_object(cJSON_DataType_t containerType);
+
+//   ---   Public Function Prototypes   ---
+
+
+
+//   ---   Private Function Implementations   ---
+
+cJSON_Generic_t *cJSON_malloc_generic_object(cJSON_DataType_t containerType)
+{
+    cJSON_Generic_t *genObj = (cJSON_Generic_t *)malloc(sizeof(cJSON_Generic_t));
+
+    genObj->type = containerType;
+
+    size_t containerSize;
+
+    // Select container pointer size
+    switch(containerType)
+    {
+    case Dictionary:
+        containerSize = sizeof(cJSON_Dict_t);
+        break;
+    case List:
+        containerSize = sizeof(cJSON_List_t);
+        break;
+    case String:
+        containerSize = sizeof(cJSON_String_t);
+        break;
+    case Integer:
+        containerSize = sizeof(cJSON_Int_t);
+        break;
+    case Float:
+        containerSize = sizeof(cJSON_Float_t);
+        break;
+    case Boolean:
+        containerSize = sizeof(cJSON_Bool_t);
+        break;
+    }
+
+    // Allocate memory for data container
+    genObj->dataStruct = malloc(containerSize);
+    
+    // Set allocated memory to 0
+    if (genObj->dataStruct != NULL) memset(genObj->dataStruct, 0, containerSize);
+    
+    return genObj;
+}
+
+//   ---   Public Function Implementations   ---
